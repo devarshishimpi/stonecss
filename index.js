bcb3f9ff0569e9cd4f289c189a6a706f051c7e83
Zeyu Yao <novodoodle@gmail.com>
2022-07-21 23:14:55 -0700

    Update CSS and content
    - Remove excessive font family declarations
    - Update site content
    - Add ripple effect on buttons

---
+++ index.js
@@ -0,0 +1,26 @@
+function createRipple(event) {
+    const button = event.currentTarget;
+  
+    const circle = document.createElement("span");
+    const diameter = Math.max(button.clientWidth, button.clientHeight);
+    const radius = diameter / 2;
+  
+    circle.style.width = circle.style.height = `${diameter}px`;
+    circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+    circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+    circle.classList.add("ripple");
+  
+    const ripple = button.getElementsByClassName("ripple")[0];
+  
+    if (ripple) {
+      ripple.remove();
+    }
+  
+    button.appendChild(circle);
+}
+  
+const buttons = document.getElementsByTagName("button");
+
+for (const button of buttons) {
+    button.addEventListener("click", createRipple);
+}
